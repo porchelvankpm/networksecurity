450c9ae92c30339ebf82def2db784b33fa7e5ce2
porchelvankpm <porchelvankpm@gmail.com>
2025-01-16 01:16:38 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import sys
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
@@ -16,12 +17,23 @@ if __name__=='__main__':
                 dataingestionartifact=data_ingestion.initiate_data_ingestion()
                 logging.info("Data Initiation Completed")
                 print(dataingestionartifact)
+
                 data_validation_config=DataValidationConfig(trainingpipelineconfig)
                 data_validation=DataValidation(dataingestionartifact,data_validation_config)
                 logging.info("Initiate the data Validation")
                 data_validation_artifact=data_validation.initiate_data_validation()
                 logging.info("data Validation Completed")
                 print(data_validation_artifact)
+
+                data_transformation_config =  DataTransformationConfig(trainingpipelineconfig)
+                data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+                logging.info("data transformation completed")
+                data_transformation_artifact = data_transformation.initiate_data_transformation()
+                logging.info("data transformation completed")
+                print(data_transformation_artifact)
+
+
+
                     
                     
                 
