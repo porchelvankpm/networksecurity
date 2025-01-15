d5ca6edc20055018104df54da86ebe532c3c619e
porchelvankpm <porchelvankpm@gmail.com>
2025-01-15 20:01:54 +0530

    Data validation completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,13 +1,25 @@
 from pymongo.mongo_client import MongoClient
 
 uri = "mongodb+srv://porchelvankpm:Porch1994@cluster0.2ldno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# uri = "mongodb://localhost:27017"
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
+# Create a new client with SSL verification disabled
+client = MongoClient(uri, tlsAllowInvalidCertificates=True)
+# client = MongoClient(uri)
+
+# from pymongo.mongo_client import MongoClient
+# import certifi
+
+# # Correct MongoDB Atlas URI
+# uri = "mongodb+srv://porchelvankpm:Porch1994@cluster0.2ldno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# # Ensure the MongoClient uses the correct URI
+# client = MongoClient(uri, tlsCAFile=certifi.where())
+
 
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+    print(f"Error: {e}")
